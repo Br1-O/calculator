20aeede8b363e647e62fb56c98e4a350f08d14b5
Br1-O <bruno.ortuno2@gmail.com>
2024-04-02 07:39:37 -0300

    added copy functionality for historyPane

--- assets/js/sessionStorage.js
+++ assets/js/sessionStorage.js
@@ -14,17 +14,56 @@
     let historyPane = document.getElementById("historyPane");
     let historyPaneOpen = false;
 
+    //Event Listener for btnHistory
     btnHistory.addEventListener("click", () => {
+        //retrieve history array from sessionStorage
         let historyValues = JSON.parse(sessionStorage.getItem(history));
-        let historyInfo =`<h3> Operaciones realizadas: </h3>`;
+        let historyInfo =`<div>
+                          <img src="./assets/images/copy.png" id="btnCopyAll" style="height: 2rem; margin-right: 0.5rem;" alt="copy icon" title="Copiar historial"></img>
+                          <h3> Operaciones realizadas: </h3>
+                          </div>`;
 
+        let idCounter = 1;
+
+        //iterate history array values
         for (const operation of historyValues) {
-            historyInfo += `<p>${operation}</p>`;
+            historyInfo += `<article>
+                            <img src="./assets/images/copy.png" class="btnCopy" id="btnCopy${idCounter}" style="height: 1.5rem; margin-right: 0.2rem;" title="Copiar operacion" alt="copy icon"></img>
+                            <p id="result${idCounter}">${operation}</p>
+                            </article>`;
+                
+            ++idCounter;
         }
-
+        
+        //Place array values in history Pane
         historyPane.innerHTML = historyInfo;
 
+        //EventListener for btnCopyAll
+        (document.getElementById("btnCopyAll")).addEventListener("click", () => {
+            navigator.clipboard.writeText(historyValues);
+        });
 
+        //Event Listener for each copy operation button 
+        if (historyValues.length > 0) {
+            let groupBtnCopy = document.querySelectorAll(".btnCopy");
+            let onlyOperation;
+
+            //iterate through group of btnCopy
+            groupBtnCopy.forEach( (btn, index) => {
+                btn.addEventListener("click", () => {;
+                    try {
+                        //trim result, so only the operation are copied for re use
+                        onlyOperation = ((document.getElementById(`result${index+1}`)).textContent).substring(0, ((document.getElementById(`result${index+1}`)).textContent).indexOf("=")); 
+                        //copy operation into clipboard
+                        navigator.clipboard.writeText(onlyOperation);
+                    } catch (error) {
+                        console.log("¡No se pudo copiar el valor!");
+                    }
+                });
+            });
+        };
+
+        //toggle function for historyPane
         if (!historyPaneOpen) {
             historyPane.style = "display: block;";
             historyPaneOpen = true;
